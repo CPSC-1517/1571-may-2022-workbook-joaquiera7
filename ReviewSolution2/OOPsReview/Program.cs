dbb621bf7e4a81af304c7495fd3e045a130b4378
joaquiera7 <97209582+joaquiera7@users.noreply.github.com>
2022-06-02 07:32:29 -0600

    commit

--- ReviewSolution2/OOPsReview/Program.cs
+++ ReviewSolution2/OOPsReview/Program.cs
@@ -9,10 +9,12 @@ Console.WriteLine("Hello, World!");
 //an instance class needs to be created using the new command and the class constructor
 //one needs to declare a variable of datatype Employment
 
-Employment myEmp = new Employment("Level 5 Programmer", SupervisoryLevel.Supervisor, 15.9); //default constructor
-Console.WriteLine(myEmp.ToString()); //use the instance name to reference items within your class
+//Employment myEmp = new Employment("Level 5 Programmer", SupervisoryLevel.Supervisor, 15.9); 
 
-myEmp.SetEmployeeResponsibilityLevel(SupervisoryLevel.DepartmentHead);
+//default constructor
+//Console.WriteLine(myEmp.ToString()); //use the instance name to reference items within your class
 
-Console.WriteLine(myEmp.ToString());
+//myEmp.SetEmployeeResponsibilityLevel(SupervisoryLevel.DepartmentHead);
+
+//Console.WriteLine(myEmp.ToString());
 
