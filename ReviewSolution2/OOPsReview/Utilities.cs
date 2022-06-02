dc37abd02f7421f91d17c3d69bfb950d68552aba
joaquiera7 <97209582+joaquiera7@users.noreply.github.com>
2022-06-02 05:46:42 -0600

    Initial commit with IsZeroPositive method

---
+++ ReviewSolution2/OOPsReview/Utilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public static class Utilities
+    {
+        //static class are NOT instantiated by the outside user (developer)
+        //static class items are referenced using the classname.xxxx
+        //method within this class have the keyword static in their signature
+        //static classes are shared between all outside user at the SAME time
+        //DO NOT consider saving data within a static class BECAUSE you cannot be certain it will
+        //  be there when you use the class again
+        //consider placing GENERIC reusable methods within a static class
+
+        //sample of overloaded methods
+        //the method signatures are different
+        public static bool IsZeroPositive(int value)
+        {
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+        public static bool IsZeroPositive (double value)
+        {
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+        public static bool IsZeroPositive(decimal value)
+        {
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
