05776436d454c5f22ebaacb03915073fa3dd1697
joaquiera7 <97209582+joaquiera7@users.noreply.github.com>
2022-06-03 05:03:51 -0600

    review of data type struct

---
+++ ReviewSolution2/OOPsReview/ResidentAddress.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview.Data
+{
+    //struct is a value variable
+    //class is reference variable
+    public struct ResidentAddress
+    {
+        //data members
+        public int Number;
+        public string Address1;
+        public string Address2;
+        private string _Unit;
+        private string _City;
+        public string ProvinceState;
+
+        //Properties
+        public string Unit
+        {
+            get { return _Unit; }
+            set { _Unit = value; }
+        }
+
+        public string City
+        {
+            get { return _City; }
+            set { _City = value; }
+        }
+
+        public ResidentAddress(int Number, string Address1, string Address2, string Unit, string City, string ProvinceState)
+        {
+            //concern:  parameter name is exactly the same as the struct/class field/property
+            //
+            //solution: use the keyword this. on your instance item
+            //
+            //the keyword this refereences to the instace that you are currently accessing in your program
+            
+            this.Number = Number;
+            this.Address1 = Address1;
+            this.Address2 = Address2;
+            this.ProvinceState = ProvinceState;
+
+            //for a property, one MUST use a fully implemented property with the data member, and assign the incoming value
+            //  to the data member instead of the property (as can be done in a class)
+
+            _Unit = Unit;
+            _City = City;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Number},{Address1},{Address2},{Unit},{City},{ProvinceState}";
+        }
+    }
+}
